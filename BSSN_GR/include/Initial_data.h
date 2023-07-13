f0aeae636c9a02ec42baf4954ae6ac3afe20d729
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-07-13 11:30:03 +0200

    Cleaning.  Need to add json parser based on ID used

--- BSSN_GR/include/Initial_data.h
+++ BSSN_GR/include/Initial_data.h
@@ -3,25 +3,24 @@
 #include "parameters.h"
 #include <memory>
 #include "fuka_bh.h"
+#include "json.hpp"
+using json = nlohmann::json;
 namespace initial_data
 {
-    // struct Initial_data {
-        // inline void operator()(double _x, double _y, double _z, double* var) {
-        inline static void import(double _x, double _y, double _z, double* var) {
-            const double xx=GRIDX_TO_X(_x);
-            const double yy=GRIDY_TO_Y(_y);
-            const double zz=GRIDZ_TO_Z(_z);
-            
-            #ifdef FUKA_BH_ID
-                fuka_id_bh_importer::FUKA_BH_XCTS fuka_bh_data("test.info");
-                fuka_bh_data(xx,yy,zz,var);
-            #else
-                bssn::punctureData(xx,yy,zz,var);
-            #endif
-        }
-    // };
-    // auto import_initial_data = [&](auto& fx) {
-    //     return 
-    // };
+    inline static void import(double _x, double _y, double _z, double* var) {
+        const double xx=GRIDX_TO_X(_x);
+        const double yy=GRIDY_TO_Y(_y);
+        const double zz=GRIDZ_TO_Z(_z);
+        
+        #ifdef FUKA_BH_ID
+            fuka_id_bh_importer::FUKA_BH_XCTS fuka_bh_data("test.info");
+            fuka_bh_data(xx,yy,zz,var);
+        #else
+            bssn::punctureData(xx,yy,zz,var);
+        #endif
+    }
+    inline static void extract_parameters(json& parfile) {
+
+    }
 } // namespace initial_data
 
